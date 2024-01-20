5f465777adff184c97d4203d13762493178210be
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-01-20 16:06:23 -0300

    feat: add a google fonts package

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/answer_button.dart';
 import 'package:flutter_quiz/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -34,7 +35,11 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white, fontSize: 16),
+              style: GoogleFonts.montserrat(
+               color: Colors.white,
+               fontSize: 24,
+               fontWeight: FontWeight.bold,
+              ),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
