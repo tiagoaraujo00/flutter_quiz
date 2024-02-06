672e287cbfed7878be1bcdba846ebe2eefbfd12c
tiagoaraujo00 <tiagodevjs@gmail.com>
2024-02-06 16:26:07 -0300

    fix: questions_summary

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,7 +11,13 @@ class QuestionsSummary extends StatelessWidget {
       children: summaryData.map((data) {
         return Row(
           children: [
-            Text(((data['question'] as int )+ 1).toString()),
+            Text(((data['question_index'] as int)+ 1).toString()),
+            Column(children: [
+              Text(data['question'] as String),
+              const SizedBox(height: 5,),
+              Text(data['user_answer'] as String),
+              Text(data['correct_answer'] as String),
+            ],),
           ],
         );
       }).toList(),
